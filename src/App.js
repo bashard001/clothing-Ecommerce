dbb364b60e29b2b202df735bd5ecac28f4ca1f96
unknown <bdaseh@gmail.com>
2021-06-30 14:10:07 -0700

    starting routing in our project using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 
 import './App.css';
-
+import { Switch, Route } from 'react-router-dom';
 import HomePage from "./pages/homepage/homepage.component"
+
+
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>
+        hats page
+      </h1>
+    </div>)
+}
 
 function App() {
   return (
     <div >
-    <HomePage />
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        <Route path="/hats">
+          <HatsPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
