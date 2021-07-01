c033d05f036cd1635fb1e6a22ddf47d3615ed77b
unknown <bdaseh@gmail.com>
2021-07-01 14:41:53 -0700

    updating our app with collection-item component and styling

--- src/components/preview-collection/preview-collection.js
+++ src/components/preview-collection/preview-collection.js
@@ -1,15 +1,16 @@
 import React from 'react'
 
-// import "./preview-collection.styles.scss"
+import CollectionItem from '../collection/collection-item'
+
+import "./preview-collection.styles.scss"
 
 const CollectionPreview = ({ title, items }) => (
     <div className="collection-preview">
         <h1 className="title">{title.toUpperCase()}</h1>
         <div className="preview">
             {items.filter((item, indx) => indx < 4)
-                .map(item => (
-                    <div key={item.id}>{item.name}
-                    </div>
+                .map(({id, ...itemProps}) => (
+                    <CollectionItem key={id} {...itemProps} />
                 ))}
         </div>
     </div>
