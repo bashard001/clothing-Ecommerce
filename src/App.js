7440adfefe13be5a9041b200c7ba2e96a3666912
unknown <bdaseh@gmail.com>
2021-07-01 13:45:38 -0700

    made a collections component that filters through an arry and them map it to our shop page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from "./pages/homepage/homepage.component"
+import ShopPage from './pages/shop/shop.component';
 
 
 
@@ -21,8 +22,8 @@ function App() {
         <Route path="/" exact>
           <HomePage />
         </Route>
-        <Route path="/hats">
-          <HatsPage />
+        <Route path="/shop">
+          <ShopPage />
         </Route>
       </Switch>
     </div>
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,17 +1,35 @@
+import React from "react"
 
 import SHOP_DATA from "./shop.data"
+
+import CollectionPreview from "../../components/preview-collection/preview-collection"
 
 
 class ShopPage extends React.Component {
 
-    constructor(){
+    constructor() {
         super()
         this.state = {
 
             collections: SHOP_DATA
-       
+
+        }
     }
-}
+    render() {
+
+        const {collections} = this.state
+        
+
+        return (<div className="shop-page">
+            {
+                collections.map(({id, ...collectionProps}) => {
+                    return <CollectionPreview key={id} {...collectionProps} />
+                })
+            }
+
+        </div>)
+
+    }
 }
 
 
