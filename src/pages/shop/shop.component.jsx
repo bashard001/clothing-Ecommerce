3b95ee352f61504101bdb609c450b47fee1db75a
unknown <bdaseh@gmail.com>
2021-07-01 13:15:25 -0700

    created a shop page and an arry of object data in a separate file to feed into the shop page

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,20 @@
+
+import SHOP_DATA from "./shop.data"
+
+
+class ShopPage extends React.Component {
+
+    constructor(){
+        super()
+        this.state = {
+
+            collections: SHOP_DATA
+       
+    }
+}
+}
+
+
+
+
+export default ShopPage
