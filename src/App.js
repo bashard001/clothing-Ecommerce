d30cf2afd6cb9ec917d9e5b3d51414b30d0feb70
unknown <bdaseh@gmail.com>
2021-07-02 17:36:07 -0700

    working on the state of our user in the application

--- src/App.js
+++ src/App.js
@@ -1,31 +1,56 @@
-
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from "./pages/homepage/homepage.component"
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header';
 import SignInSignOutPage from './pages/sign-in-up/sign-in-out';
+import { auth } from "./firebase/firebase.utils";
+import React from 'react';
 
 
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      currentUser: null
+    }
+  }
+  unsubscribeFromAuth = null
+
+  componentDidMount(){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user })
+
+      console.log(user)
+    })
+  }
 
 
-function App() {
-  return (
-    <div >
-      <Header />
-      <Switch>
-        <Route path="/" exact>
-          <HomePage />
-        </Route>
-        <Route path="/shop">
-          <ShopPage />
-        </Route>
-        <Route path="/signin">
-          <SignInSignOutPage />
-        </Route>
-      </Switch>
-    </div>
-  );
+componentWillUnmount(){
+  this.unsubscribeFromAuth()
+}
+
+  render() {
+    return (
+      <div >
+        <Header />
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          <Route path="/shop">
+            <ShopPage />
+          </Route>
+          <Route path="/signin">
+            <SignInSignOutPage />
+          </Route>
+        
+        </Switch>
+      </div>
+    );
+  }
+
 }
 
 export default App;
