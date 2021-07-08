0445308cadfc16b1de149aaf46617790d5565b99
unknown <bdaseh@gmail.com>
2021-07-08 11:27:18 -0700

    finished redux set up and config

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import App from './App';
 import { BrowserRouter } from "react-router-dom"
 import { Provider } from "react-redux"
 
+import store from "./redux/store"
+
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
         <App />
