6512e45b9ea73a96e07c5197bc18b3025ecdd71f
unknown <bdaseh@gmail.com>
2021-07-20 12:26:33 -0700

    updating out dropdown cart with selectors with memoized selector

--- src/components/cart-dropdown/cart-dropdown.js
+++ src/components/cart-dropdown/cart-dropdown.js
@@ -5,6 +5,8 @@ import CustomButton from '../custom-button/custom-button.component'
 
 import CartItem from '../cart-item/cart-item'
 
+import { selectCartItems } from "../../redux/cart/cart.selector";
+
 import "./cart-dropdown.scss"
 
 const CartDropdown = ({cartItems}) => (
@@ -16,8 +18,8 @@ const CartDropdown = ({cartItems}) => (
     </div>
 )
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-    cartItems
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropdown) 
