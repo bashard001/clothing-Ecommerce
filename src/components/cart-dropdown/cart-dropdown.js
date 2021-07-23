92c666c6f402976a7417626afee6d969d0bf7b38
unknown <bdaseh@gmail.com>
2021-07-23 10:11:52 -0700

    improved user flow from cart dropdown to the checkout page buy using redux dispatch to toggle cart hidden

--- src/components/cart-dropdown/cart-dropdown.js
+++ src/components/cart-dropdown/cart-dropdown.js
@@ -8,9 +8,11 @@ import CartItem from '../cart-item/cart-item'
 
 import { selectCartItems } from "../../redux/cart/cart.selector";
 
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
+
 import "./cart-dropdown.scss"
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
     <div className="cart-dropdown">
         <div className="cart-items">
             {
@@ -19,7 +21,10 @@ const CartDropdown = ({ cartItems, history }) => (
                     : <span className="empty-message">Your cart is empty</span>
             }
         </div>
-        <CustomButton onClick={() => history.push("/checkout")}>GO TO CHECKOUT</CustomButton>
+        <CustomButton onClick={() => {
+            history.push("/checkout");
+            dispatch(toggleCartHidden())
+            }}>GO TO CHECKOUT</CustomButton>
     </div>
 )
 
