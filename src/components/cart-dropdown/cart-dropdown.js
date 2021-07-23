6ab1e832a03da42b66c36b0b02cdc8dfd12ac614
unknown <bdaseh@gmail.com>
2021-07-22 17:53:01 -0700

    working on the checkout page and added some styling

--- src/components/cart-dropdown/cart-dropdown.js
+++ src/components/cart-dropdown/cart-dropdown.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 
 import CustomButton from '../custom-button/custom-button.component'
 
@@ -9,7 +10,7 @@ import { selectCartItems } from "../../redux/cart/cart.selector";
 
 import "./cart-dropdown.scss"
 
-const CartDropdown = ({ cartItems }) => (
+const CartDropdown = ({ cartItems, history }) => (
     <div className="cart-dropdown">
         <div className="cart-items">
             {
@@ -18,7 +19,7 @@ const CartDropdown = ({ cartItems }) => (
                     : <span className="empty-message">Your cart is empty</span>
             }
         </div>
-        <CustomButton>GO TO CHECKOUT</CustomButton>
+        <CustomButton onClick={() => history.push("/checkout")}>GO TO CHECKOUT</CustomButton>
     </div>
 )
 
@@ -26,4 +27,4 @@ const mapStateToProps = (state) => ({
     cartItems: selectCartItems(state)
 })
 
-export default connect(mapStateToProps)(CartDropdown)
+export default withRouter(connect(mapStateToProps)(CartDropdown)) 
--- src/pages/checkout/checkout.js
+++ src/pages/checkout/checkout.js
@@ -1,10 +1,28 @@
 import React from 'react'
-
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectCartItems } from "../../redux/cart/cart.selector";
 import "./checkout.styles.scss"
 
 const CheckoutPage = () => (
-    <div>
-        CHECKOUT PAGE
+    <div className="checkout-page">
+        <div className="checkout-header">
+            <div className="header-block">
+                <span>Product</span>
+            </div>
+            <div className="header-block">
+                <span>Description</span>
+            </div>
+            <div className="header-block">
+                <span>Quantity</span>
+            </div>
+            <div className="header-block">
+                <span>Price</span>
+            </div>
+            <div className="header-block">
+                <span>Remove</span>
+            </div>
+        </div>
     </div>
 )
 
