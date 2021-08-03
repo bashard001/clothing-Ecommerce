d526138b21423036e923c4285da9b2fa99fcdf0a
unknown <bdaseh@gmail.com>
2021-08-02 20:29:58 -0700

    styles update

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import { ReactComponent as Logo } from "../../assets/crown.svg"
+import { ReactComponent as Logo } from "../../assets/chess-queen-solid.svg"
 import "./header.styles.scss"
 import { auth } from "../../firebase/firebase.utils"
 import { connect } from "react-redux";
