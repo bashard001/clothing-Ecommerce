06763cd2d28c33827c9805d9df40516fe44764dc
unknown <bdaseh@gmail.com>
2021-08-03 01:07:32 -0700

    styling update

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 
 import SHOP_DATA from "./shop.data"
+import "./shop-page.style.css"
 
 import CollectionPreview from "../../components/preview-collection/preview-collection"
 
