5943e7cd92e1f7c029132ec2a56c90ace0bd28b4
unknown <bdaseh@gmail.com>
2021-08-16 12:28:46 -0700

    creating nested routing in shop page

---
+++ src/pages/category/category.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import collectionItem from '../../components/collection/collection-item';
+import "./category.styles.scss"
+
+
+const CategoryPage = ({match}) => {
+    
+    console.log(match)
+
+    return (
+    <div className="category">
+        <h2>CATEGORY PAGE</h2>
+    </div>
+)
+}
+
+export default CategoryPage
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,14 +1,17 @@
 import React from "react"
 import CollectionOverview from "../../components/collections-overview/collections-overview"
 import { Route } from "react-router-dom"
+import CategoryPage from "../category/category"
 import "./shop-page.style.css"
 
 
 
-const ShopPage =  () => {
+const ShopPage =  ({ match }) => {
+    console.log(match)
         
         return (<div className="shop-page">
-           <Route exact path={} component={ CollectionOverview }/>
+           <Route exact path={`${match.path}`} component={ CollectionOverview }/>
+           <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
         </div>)
     }
 
