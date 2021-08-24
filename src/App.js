50e20d13ce07756c723877bd22b9737ded705dc7
bashard001 <bdaseh@gmail.com>
2021-08-24 08:47:53 -0700

    spacing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import SignInSignOutPage from './pages/sign-in-up/sign-in-out';
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import React from 'react';
 import { connect } from "react-redux";
-import { setCurrentUser } from "./redux/user/user.actions";
+import { setAsGuest, setCurrentUser } from "./redux/user/user.actions";
 import CheckoutPage from './pages/checkout/checkout';
 import Footer from './components/footer/footer';
 import SignInPopup from './components/sign-in-popup/sign-in-popup';
@@ -77,7 +77,8 @@ const mapStateToProps = ({user})=>({
   isGuest: user.isGuest
 })
 const mapDispatchToProps = dispatch => ({
-setCurrentUser: user => dispatch(setCurrentUser(user))
+setCurrentUser: user => dispatch(setCurrentUser(user)),
+setAsGuest: user => dispatch(setAsGuest(user))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps )(App);
