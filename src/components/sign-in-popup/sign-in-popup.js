b21da07f1700d7c1d2b87b1cbfd64206227adc54
bashard001 <bdaseh@gmail.com>
2021-08-23 20:43:23 -0700

    added a sign in card in the pop up

--- src/components/sign-in-popup/sign-in-popup.js
+++ src/components/sign-in-popup/sign-in-popup.js
@@ -1,10 +1,13 @@
 import React from "react"
 import"./sign-in-popup.styles.scss"
+import SignIn from "../sign-in/sign-in"
 
 
 const SignInPopup = ()=>(
     <div className="popup">
-        
+        <div className="popupCard">
+<SignIn />
+        </div>
     </div>
 )
 
