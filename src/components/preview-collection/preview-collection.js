d34a33288330c4c3b663e778613a94b6b73fe469
unknown <bdaseh@gmail.com>
2021-08-28 17:47:28 -0700

    responsive app

--- src/components/preview-collection/preview-collection.js
+++ src/components/preview-collection/preview-collection.js
@@ -6,7 +6,7 @@ const CollectionPreview = ({ title, items }) => (
     <div className="collection-preview">
         <h1 className="title">{title.toUpperCase()}</h1>
         <div className="preview">
-            {items.filter((item, indx) => indx < 4)
+            {items.filter((item, indx) => indx < 6)
                 .map((item) => (
                     <CollectionItem key={item.id} item={item} />
                 ))}
